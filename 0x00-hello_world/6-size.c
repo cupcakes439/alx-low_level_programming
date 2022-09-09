2da6414e1c5dd5c86ea3ca8b6a1693366242b4b4
cupcakes439 <adeolaakande439@gmail.com>
2022-09-09 08:56:36 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Description: 'use of sizeof( )'
  * Return: Always 0 (Success)
  */
- int main(void)
+int main(void)
 {
-	char age;
-	int aged;
-	long int ages;
-	long long int ag;
-	float agem;
+char age;
+int aged;
+long int ages;
+long long int ag;
+float agem;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(age));
-	printf("Size of an int: %zu byte(s)\n", sizeof(aged));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(ages));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(ag));
-	printf("Size of a float: %zu byte(s)\n", sizeof(agem));
-
-	return (0);
+printf("Size of a char: %zu byte(s)\n", sizeof(age));
+printf("Size of an int: %zu byte(s)\n", sizeof(aged));
+printf("Size of a long int: %zu byte(s)\n", sizeof(ages));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(ag));
+printf("Size of a float: %zu byte(s)\n", sizeof(agem));
+return (0);
 }	
