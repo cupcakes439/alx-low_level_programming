9892f8eb880a96d03533b765cbe9051c6341ce11
cupcakes439 <adeolaakande439@gmail.com>
2022-09-08 22:06:18 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 include <stdio.h>
-
 /**
  * main - Entry point
  * Description: 'use of sizeof( )'
@@ -18,5 +17,6 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
 	return(0);
 }	
