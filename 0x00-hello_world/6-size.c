9487afb9b358c536aee68373c3caedb480fe02a4
cupcakes439 <adeolaakande439@gmail.com>
2022-09-08 22:40:38 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int ag;
 	float agem;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(age));
-	printf("Size of an int: %zu byte(s)\n", sizeof(aged));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(ages));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(ag));
-	printf("Size of a float: %zu byte(s)\n", sizeof(agem));
+	printf("Size of a char: %zu byte (s)\n", sizeof(age));
+	printf("Size of an int: %zu byte (s)\n", sizeof(aged));
+	printf("Size of a long int: %zu byte (s)\n", sizeof(ages));
+	printf("Size of a long long int: %zu byte (s)\n", sizeof(ag));
+	printf("Size of a float: %zu byte (s)\n", sizeof(agem));
 	return (0);
 }	
