5c37ec528145aa6017800635ab60ed649c44d36d
cupcakes439 <adeolaakande439@gmail.com>
2022-09-08 21:47:44 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point
- * Description: 'using printf'
+ * 
  * Return: Always 0 (success)
  */
- int main(void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a place of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
