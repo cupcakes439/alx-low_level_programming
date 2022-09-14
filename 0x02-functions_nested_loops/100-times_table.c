4f58a93dadef6f472db89deb690ad1f913bbecd8
cupcakes439 <adeolaakande439@gmail.com>
2022-09-14 09:54:51 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-/**
- * print_times_table - Entry point
- * @n: input
- * Return: Always 0 (Success)
- */
-void print_times_table(int n)
-{
+#include "main.h"
 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_times_table(2);
 	return (0);
 }
