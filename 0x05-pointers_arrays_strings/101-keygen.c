9d58e2c7c2fad74c1c04c14c4ab75f678f60f165
cupcakes439 <adeolaakande439@gmail.com>
2022-09-19 16:24:01 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,27 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include "main.h"
-
+#import "holberton.h"
 /**
- * main - entry point
- *
- * Return: generated password
+ * main -
+ * Return:
  */
-
 int main(void)
 {
-	char c;
-	int x;
-
-	srand(time(0));
-	while (x <= 2645)
-	{
-		c = rand() % 128;
-		x += c;
-		putchar(c);
-	}
-	putchar(2772 - x);
-
 	return (0);
 }
