492bb3b89783ee4234b65d2744464d62652833a7
cupcakes439 <adeolaakande439@gmail.com>
2022-10-06 07:56:01 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,10 @@
+#include "main.h"
 #include <stdlib.h>
+
 /**
   * malloc_checked - allocates memory using malloc
   * @b: size of buffer
+  *
   * Return: pointer to buffer
   */
 void *malloc_checked(unsigned int b)
