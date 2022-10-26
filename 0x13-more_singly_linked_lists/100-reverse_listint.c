70479c8f1184022a618816d3497cf214ce6a99fb
cupcakes439 <adeolaakande439@gmail.com>
2022-10-26 15:07:21 -0700

    Task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,34 +1,11 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
-  * delete_nodeint_at_index - delete a node at index
-  * @head: head of the list
-  * @index: location of the node to delete
-  * Return: pointer to head of list
+  * reverse_listint - define function
+  * @head: describe argument
+  * Return: what does it return?
   */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *reverse_listint(listint_t **head)
 {
-	unsigned int i;
-	listint_t *current, *subsequent;
-
-	if (!head || !*head)
-		return (-1);
-	current = *head;
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(current);
-		return (1);
-	}
-	for (i = 0; i < (index - 1); i++)
-	{
-		current = current->next;
-		if (current == NULL)
-			return (-1);
-	}
-	subsequent = current->next;
-	current->next = subsequent->next;
-	free(subsequent);
-	return (1);
+	head++;
+	return (0);
 }
