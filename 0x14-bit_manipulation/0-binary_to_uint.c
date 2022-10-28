d449fd3b2fd9848d8a50fd3f49605edc013ecc99
cupcakes439 <adeolaakande439@gmail.com>
2022-10-28 01:15:28 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,46 +1,37 @@
-#include "holberton.h"
-/**
-  * _stoi - converts chars to ints
-  * @c: char to convert
-  * Return: converted int
-  */
-unsigned int _stoi(char c)
-{
-	return ((unsigned int) c - '0');
-}
-/**
-  * _strlen - calculates the length of the string
-  * @s: input
-  * Return: length of string
-  */
-unsigned int _strlen(const char *s)
-{
-	unsigned int i;
+#include "main.h"
 
-	for (i = 0; s[i]; i++)
-		;
-	return (i);
-}
 /**
-  * binary_to_uint - converts a string of 1's and 0's to a decimal number
-  * @b: string to convert
-  * Return: unsigned decimal number
-  */
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int result, tmp, expo;
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
-	result = tmp = 0;
-	expo = 1;
-	for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		tmp = _stoi(b[i]);
-		result += tmp * expo;
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (result);
+
+	return (ui);
 }
